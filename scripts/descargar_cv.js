bcd66da598fbcfa05b8c0ee6069c532d7e46bf7c
acardom <cardenoalberto@gmail.com>
2025-02-19 19:58:03 +0100

    novedad

--- scripts/descargar_cv.js
+++ scripts/descargar_cv.js
@@ -1,6 +1,6 @@
 document.getElementById('download-cv').addEventListener('click', function() {
     const link = document.createElement('a');
-    link.href = 'archivos/cv_practicas.pdf';
+    link.href = 'archivos/cv_AlbertoCárdeno.pdf';
     link.download = 'CV-Alberto_Cárdeno.pdf';
     link.target = '_blank'; // Abre el enlace en una nueva pestaña
     document.body.appendChild(link);
